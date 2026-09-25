20cce21467f21b5040e2909406833d80e4925af4
web4 <lunghugh@gmail.com>
2026-09-25 12:11:38 +0100

    Update config.nix

--- bin/config.nix
+++ bin/config.nix
@@ -3,7 +3,7 @@ $git config --list
 $ git config --global --list
 $ git config --global user.name "seriki yakub"
 github$ git config --global user.email "author@github.com"
-github$ git config --global user.email "author@gitlab.com"
+github$ git config --global user.email "author@gh.io"
 github$ git config --global user.email "author@git-scm.com"
 git config user.email "author+CO2NWYH32WPRO7DXWTDXUCDLFZCIFEVBNHHQAAAAAFGK6KHH@user.github.com
 $ git branch --all
