cf3bee667a142fc5d9a2138da62072d40a20368e
web4 <lunghugh@gmail.com>
2026-09-25 12:10:21 +0100

    Update and rename config.sh to config.nix

---
+++ bin/config.nix
@@ -0,0 +1,10 @@
+git config --global http.proxy https://web4hub.github.io:8080
+$git config --list
+$ git config --global --list
+$ git config --global user.name "seriki yakub"
+github$ git config --global user.email "author@github.com"
+github$ git config --global user.email "author@gitlab.com"
+github$ git config --global user.email "author@git-scm.com"
+git config user.email "author+CO2NWYH32WPRO7DXWTDXUCDLFZCIFEVBNHHQAAAAAFGK6KHH@user.github.com
+$ git branch --all
+$ git branch -
