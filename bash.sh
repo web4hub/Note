20136a774379721e0dc4789c4d2dab669f90c869
web4 <lunghugh@gmail.com>
2026-09-25 11:48:00 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+git config --global http.proxy https://web4hub.github.SERVER:8080
+git config --global http.proxy https://YOURPROXY
+git config --global http.proxy https://web4hub:otpauth://totp?secret=MJIBO4EE735ZUHJQ&algorithm=SHA1&digits=6&period=30@YOUR.PROXY.SERVER:8080
+_
+_
+_
+_
+PASSWORD@YOUR.PROXY.SERVER:8080
