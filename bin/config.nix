5cc75fc6f9dfccf4c5c02f141f090f4f1c901224
web4 <lunghugh@gmail.com>
2026-09-26 05:47:55 +0100

    Update config.nix

--- bin/config.nix
+++ bin/config.nix
@@ -1,10 +1,9 @@
-git config --global http.proxy https://web4hub.github.io:8080
+git config --global http.proxy https://g4.proxy-man.com:5959
 $git config --list
 $ git config --global --list
 $ git config --global user.name "seriki yakub"
 github$ git config --global user.email "author@github.com"
 github$ git config --global user.email "author@gh.io"
 github$ git config --global user.email "author@git-scm.com"
-git config user.email "author+CO2NWYH32WPRO7DXWTDXUCDLFZCIFEVBNHHQAAAAAFGK6KHH@user.github.com
 $ git branch --all
 $ git branch -
